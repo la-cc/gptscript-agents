b3034140456883e3f9bc97a7812c8f9a938051e2
Artem Lajko <110809333+la-cc@users.noreply.github.com>
2024-08-17 22:42:08 +0200

    allow download multiple agents file from outside

--- build/docker/scripts/entrypoint.sh
+++ build/docker/scripts/entrypoint.sh
@@ -3,8 +3,11 @@
 # Function to download files if OUTSIDE_AGENTS_FILES is set
 download_outside_agent_files() {
     if [ -n "$OUTSIDE_AGENTS_FILES" ]; then
+        # Set IFS to comma to split the URLs, and remove any leading/trailing whitespace
         IFS=',' read -ra URLS <<<"$OUTSIDE_AGENTS_FILES"
         for url in "${URLS[@]}"; do
+            # Trim any leading or trailing spaces from each URL
+            url=$(echo "$url" | xargs)
             echo "Downloading agent file from $url..."
             curl -o /home/agentuser/gpt-agents/$(basename $url) "$url"
         done
